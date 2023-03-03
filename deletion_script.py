1f95a1ff191ba49d4a167aafb32f1fdf1ea3e27e
Justin Deng <justin.n.deng@gmail.com>
2023-03-03 15:52:22 -0700

    threaded process

--- deletion_script.py
+++ deletion_script.py
@@ -1,17 +1,20 @@
 import csv 
 import requests
+from threading import Thread
 
+username = 'justindeng555@gmail.com'
+password = 'Draven817678!'
 url = 'https://privacyapi.evidon.com/api/v3/siteNotice/'
-password = ""
-username = ""
+filename = 'test.csv'
+
 
 
 # Object created to store noticeIDs, response code, and requestURL as private members
 class Notice:
     def __init__(self,noticeID):
         self.noticeID = noticeID
-        self.responseCode = -1
-        self.requestURL = url+self.noticeID
+        self.response = None
+        self.requestURL = url+self.noticeID+'/delete'
         return
     
     def getNoticeId(self):
@@ -20,55 +23,89 @@ class Notice:
     def getRequestURL(self):
         return self.requestURL
     
-    def getResponseCode(self):
-        return self.responseCode
+    def setResponse(self,response):
+        self.response = response
+        return
     
-    def setResponseCode(self, responseCode):
-        self.responseCode = responseCode
+    def getResponse(self):
+        return self.response
+    
+
+
+    
 
 # Parses CSV
 # Creates a Notice object for each row
-# Stores Notice object in a queue
-# Returns queue
+# Stores Notice object in a array
+# Returns array of notice objects
 def parseCSV():
-    counter = 0
-    queue = []
-    with open('test.csv', newline='') as csvfile:
+    notices = []
+    with open(filename, newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             newNotice = Notice(row[0])
-            queue.append(newNotice)
-    return queue
+            notices.append(newNotice)
+    return notices
 
-# Recursice function
-# Loops through array and make api call for each object
-# If the response code is not 200 or if there is an error, then the notice object is placed in a new queue
-# Fucntion is called again until there is no objects left in queue
-# Assume all noticeID are valid
-def deleteNotice(queue):
-    if len(queue) == 0:
-        return 
-     
-    failedDeletions = []
-    for notice in queue:
-        try:
-            deletionRequest = requests.get(notice.getRequestURL(),auth=(username,password))
-            notice.setResponseCode(deletionRequest.status_code)
-            print(deletionRequest.status_code)
-            if deletionRequest.status_code != 200:
-                failedDeletions.append(notice)
-            
-        except:
-            failedDeletions.append(notice)
+# Makes an API call to delete a notice given a single noticeID
+# Returns a response Code if there is not a connection error
+# If there is a connection error, then "Connection Failed"
+def deleteNotice(notice):
+    try:
+        request = requests.get(notice.getRequestURL(), auth=(username,password))
+        return request.status_code
 
-    deleteNotice(failedDeletions)
+    except requests.exceptions.ConnectionError as err:
+        return "Connection Failed"
+    
 
-#int main
+# Makes multiple calls to deleteNotice() given an array of noticeIDs
+# Updates the private member, response, for each object.
+def deleteNoticesByArray(noticeArr,responses):
+    for notice in noticeArr:
+        res = deleteNotice(notice)
+        notice.setResponse(res)
+        responses[notice.getNoticeId()] = res
+    return responses
+
+
+
+# Creates N threads
+# Divides the notice array into multiple subset
+# Each thread is assigned a subset of Notices to delete
+def threadedDeletion(nThreads, noticeArr):
+    if len(noticeArr) == 0:
+        return
+    print('threadedDeletion Called.','notice count:',len(noticeArr))
+    threads = []
+    responses = {}
+    for i in range(nThreads):
+        noticeSubset = noticeArr[i::nThreads]
+        t = Thread(target=deleteNoticesByArray, args=(noticeSubset,responses))
+        threads.append(t)
+
+    [ t.start() for t in threads ]
+    [ t.join() for t in threads ]
+
+    newQueue = []
+    for notice in noticeArr:
+        if notice.getResponse() != 200:
+            newQueue.append(notice)
+
+    threadedDeletion(64,newQueue)
+
+    return
+
 def main():
     queue = parseCSV()
-    deleteNotice(queue)
-    return
+    threadedDeletion(64,queue)
 
 
 main()
+
+
+
+
+
+
 
